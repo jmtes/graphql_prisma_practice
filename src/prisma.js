e05090dbe2dee23a0cf423024a83262b2e13f769
jmtes <juliennetesoro@gmail.com>
2020-09-19 16:54:14 -0700

    Check if resources exist and remove redundant code

--- src/prisma.js
+++ src/prisma.js
@@ -6,33 +6,35 @@ const prisma = new Prisma({
 });
 
 const logData = (data) => console.log(JSON.stringify(data, undefined, 2));
-const logErr = (err) => console.log(err);
+const logErr = (err) => console.log(err.message);
 
 const createPostForUser = async (userId, data) => {
-  await prisma.mutation.createPost({
-    data: {
-      ...data,
-      author: {
-        connect: {
-          id: userId
+  // Make sure user exists
+  const userExists = await prisma.exists.User({ id: userId });
+  if (!userExists) throw Error('User does not exist.');
+
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: userId
+          }
         }
       }
-    }
-  });
-
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: userId
-      }
     },
-    '{ id name email posts { id title published } }'
+    '{ author { id name email posts { id title published } } }'
   );
 
-  return user;
+  return post.author;
 };
 
 const updatePostForUser = async (postId, data) => {
+  // Check if post exists
+  const postExists = await prisma.exists.Post({ id: postId });
+  if (!postExists) throw Error('Post does not exist.');
+
   const post = await prisma.mutation.updatePost(
     {
       where: {
@@ -40,29 +42,30 @@ const updatePostForUser = async (postId, data) => {
       },
       data
     },
-    '{ author { id } }'
+    '{ author { id name email posts { id title published } } }'
   );
 
-  const { id } = post.author;
-
-  const user = await prisma.query.user(
-    {
-      where: {
-        id
-      }
-    },
-    '{ id name email posts { id title published } }'
-  );
-
-  return user;
+  return post.author;
 };
 
-updatePostForUser('ckfaag2la008e0884ok0rdgjr', { published: true }).then(
-  logData
-);
+// Check if user with given ID has a post with the given title
+// prisma.exists
+//   .User({
+//     id: 'ckf8mz6jj008h0884q6tyw2v6',
+//     posts_some: {
+//       title: 'just gave myself a bad dye job'
+//     }
+//   })
+//   .then(logData);
 
-createPostForUser('ckf8mz6jj008h0884q6tyw2v6', {
-  title: 'cuffed jeans are the mark of bisexuality',
-  body: 'i dont make the rules',
+createPostForUser('ckf8mz6jj008h0884q6tyw2v', {
+  title: 'i got a crush on u.....',
+  body: '',
   published: false
-}).then(logData);
+})
+  .then(logData)
+  .catch(logErr);
+
+updatePostForUser('ckfaag2la008e0884ok0rdgj', { published: false })
+  .then(logData)
+  .catch(logErr);
