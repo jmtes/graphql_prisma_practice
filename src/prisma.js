88a9e5a5bba131e4c88a4498869de380186e41f0
jmtes <juliennetesoro@gmail.com>
2020-09-19 16:28:53 -0700

    Use async/await in bindings instead of chained promises

--- src/prisma.js
+++ src/prisma.js
@@ -8,18 +8,61 @@ const prisma = new Prisma({
 const logData = (data) => console.log(JSON.stringify(data, undefined, 2));
 const logErr = (err) => console.log(err);
 
-prisma.mutation
-  .updatePost({
-    where: {
-      id: 'ckfa9l9hg00610884tcw41fd8'
-    },
+const createPostForUser = async (userId, data) => {
+  await prisma.mutation.createPost({
     data: {
-      body: 'why wont anyone ask me out!!! im so lonely!!!',
-      published: true
+      ...data,
+      author: {
+        connect: {
+          id: userId
+        }
+      }
     }
-  })
-  .then(() =>
-    prisma.query.posts(null, '{ id title body published author { name } }')
-  )
-  .then(logData)
-  .catch(logErr);
+  });
+
+  const user = await prisma.query.user(
+    {
+      where: {
+        id: userId
+      }
+    },
+    '{ id name email posts { id title published } }'
+  );
+
+  return user;
+};
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost(
+    {
+      where: {
+        id: postId
+      },
+      data
+    },
+    '{ author { id } }'
+  );
+
+  const { id } = post.author;
+
+  const user = await prisma.query.user(
+    {
+      where: {
+        id
+      }
+    },
+    '{ id name email posts { id title published } }'
+  );
+
+  return user;
+};
+
+updatePostForUser('ckfaag2la008e0884ok0rdgjr', { published: true }).then(
+  logData
+);
+
+createPostForUser('ckf8mz6jj008h0884q6tyw2v6', {
+  title: 'cuffed jeans are the mark of bisexuality',
+  body: 'i dont make the rules',
+  published: false
+}).then(logData);
