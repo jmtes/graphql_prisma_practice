5848e67d514e0365825fed3af81029500ebe61d9
jmtes <juliennetesoro@gmail.com>
2020-09-19 21:59:34 -0700

    Remove custom resolvers

--- src/resolvers/Comment.js
+++ src/resolvers/Comment.js
@@ -1,14 +1,3 @@
-const Comment = {
-  author(parent, args, { db }, info) {
-    const { users } = db;
-
-    return users.find((user) => user.id === parent.author);
-  },
-  post(parent, args, { db }, info) {
-    const { posts } = db;
-
-    return posts.find((post) => post.id === parent.post);
-  }
-};
+const Comment = {};
 
 export default Comment;
--- src/resolvers/Post.js
+++ src/resolvers/Post.js
@@ -1,14 +1,3 @@
-const Post = {
-  author(parent, args, { db }, info) {
-    const { users } = db;
-
-    return users.find((user) => user.id === parent.author);
-  },
-  comments(parent, args, { db }, info) {
-    const { comments } = db;
-
-    return comments.filter((comment) => comment.post === parent.id);
-  }
-};
+const Post = {};
 
 export default Post;
