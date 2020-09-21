058b1807ae35861924aaae2a7fd589230d8fc091
jmtes <juliennetesoro@gmail.com>
2020-09-20 17:41:07 -0700

    Set Prisma secret

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466'
+  endpoint: 'http://localhost:4466',
+  secret: 'wAQfFMm8RGVpXh9HCjnVX15dpBF9iJCe'
 });
 
 export default prisma;
