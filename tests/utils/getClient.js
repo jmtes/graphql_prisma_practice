e559c65afc5c2ee47a3e19f251f291010579b5be
jmtes <juliennetesoro@gmail.com>
2020-09-25 11:42:25 -0700

    Set authorization header

--- tests/utils/getClient.js
+++ tests/utils/getClient.js
@@ -1,5 +1,14 @@
 import ApolloBoost from 'apollo-boost';
 
-const getClient = () => new ApolloBoost({ uri: 'http://localhost:4000' });
+const getClient = (jwt) =>
+  new ApolloBoost({
+    uri: 'http://localhost:4000',
+    request: (op) => {
+      if (jwt)
+        op.setContext({
+          headers: { Authorization: `Bearer ${jwt}` }
+        });
+    }
+  });
 
 export default getClient;
